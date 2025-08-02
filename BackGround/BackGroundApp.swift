dc1ee37e36b156a83ba96602592e8f6ab12dd291
sladezhang <sladezhang@tencent.com>
2025-08-02 23:16:55 +0800

    Initial Commit

---
+++ BackGround/BackGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackGroundApp.swift
+//  BackGround
+//
+//  Created by 张徐 on 2025/8/2.
+//
+
+import SwiftUI
+
+@main
+struct BackGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackGround/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BackGround
+//
+//  Created by 张徐 on 2025/8/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
